c147f0cf62c29e02b684ff9b1128fc8e7bbc6724
HossamTohamy1 <tohamytohamy32@gmail.com>
2025-06-18 22:54:11 +0300

    update

---
+++ HotelArchitecture.Core/Entities/Offer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Core.Entities
+{
+    public class Offer : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<OfferRoom> OfferRooms { get; set; }
+    }
+}
---
+++ HotelArchitecture.Core/Entities/OfferRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Core.Entities
+{
+    public class OfferRoom : BaseEntity
+    {
+        public int OfferId { get; set; }
+        public int RoomId { get; set; }
+        public decimal? SpecialPrice { get; set; }
+
+        public Offer Offer { get; set; }
+        public Room Room { get; set; }
+    }
+}
